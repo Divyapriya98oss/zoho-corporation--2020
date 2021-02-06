da425510ad19da66fb8b0ae55f8307e15b32c06d
Divyapriya98oss <77481702+Divyapriya98oss@users.noreply.github.com>
2021-02-06 09:40:23 +0530

    Add files via upload

---
+++ zoho.java
@@ -0,0 +1,17 @@
+class A
+{
+Public static void main(String[] args){
+for(int i=5;i>0;i--)
+{
+for(int j=5;j>i;j--)
+{
+System.out.println(j);
+}
+for(int j=i;j>0;j--)
+{
+System.out.println(i);
+}
+System.out.println();
+}
+}
+}
